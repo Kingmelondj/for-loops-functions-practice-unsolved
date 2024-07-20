01b520d2bb418c1bed530b5c1bc154c638d4ab9a
kingmelondj <kingmelondj@gmail.com>
2024-07-19 19:50:26 -0600

    Finished e9.js

--- exercises/e9.js
+++ exercises/e9.js
@@ -1,14 +1,18 @@
-
 // EXERCISE 9
 // Return an array of bank accounts with balance over $100
 // Array example: bankAccounts in /data/data.js
 // getClientsWithBalanceOverOneHundred(array) => [{ name: 'Name1', balance: 32, ... }, { name: 'Name2', balance: 3523, ... }]
 
 export function getClientsWithBalanceOverOneHundred(array) {
-  // Your code goes here...
-
-}
-
+  // Your code goes here..
+  let balanceOverOneHundred = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].balance > 100){ 
+      balanceOverOneHundred.push(array[i]);
+    }
+  }
+  return balanceOverOneHundred;
+  }
 
 
 // === TEST YOURSELF ===
